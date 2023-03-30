0365d2bc9d39f8982df0dfe38fc3b227d523c210
amitdubey <amitdubey6261@gmail.com>
2023-03-30 13:16:49 +0530

    resizing

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-import * as tf from "@tensorflow/tfjs";
+import * as tf from '@tensorflow/tfjs'
 
-/* batch of 4  , 5X5 RGB image ---->
+const newSize = [768, 560]
+const lgi = document.getElementById("lgi");
+const nnCavas = document.getElementById("nnCanvas");
 
-//batch increase the dimension ex :
+console.log()
+const blCavas = document.getElementById("blCanvas");
+const gantTensor = tf.browser.fromPixels(lgi);
 
+const nnResizeTensor = tf.image.resizeNearestNeighbor(
+    gantTensor,
+    newSize,
+    true
+).asType('int32')
 
-[
-    [120 , 120 , 3],
-    [120 , 120 , 3],
-    [120 , 120 , 3],
-    [120 , 120 , 3],
-]
-
-an extra bracket is require to grab all thus 3d become 4d ......
-
-*/
-
-const cakeImg = document.getElementById("gant")
-const canvas = document.getElementById("canvas")
-
-const flipCake = tf.tidy(()=>{
-    const cakeTensor = tf.expandDims(
-        tf.browser.fromPixels(cakeImg).asType('float32')
-    );
-    return tf.squeeze(tf.image.flipLeftRight(cakeTensor)).asType("int32")
+tf.browser.toPixels(nnResizeTensor , nnCavas).then(()=>{
+    nnResizeTensor.dispose();
 })
 
-tf.browser.toPixels(flipCake , canvas).then(()=>{
-    flipCake.dispose()
+const blResizeTensor = tf.image.resizeBilinear(
+    gantTensor,
+    newSize,
+    true
+).asType('int32')
+
+tf.browser.toPixels(blResizeTensor, blCavas).then(() => {
+    nnResizeTensor.dispose();
 })
